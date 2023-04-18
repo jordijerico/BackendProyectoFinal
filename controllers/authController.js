c4958dfe3a25b616159a1b6f9ed403aa7ecace46
jordijerico <jordijerico@gmail.com>
2023-04-18 16:34:03 +0200

    Login done succesfully

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ const authController = {};
 const { User } = require('../models')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
+// Register function
 authController.register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
@@ -34,6 +36,53 @@ authController.register = async (req, res) => {
             {
                 success: false,
                 message: "Something went wrong",
+                error: error.message
+            }
+        );
+    }
+}
+
+//Log in function
+
+authController.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email: email } });
+        if (!user) {
+
+            return res.send("Wrong Credentials");
+        }
+
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if (!isMatch) {
+
+            return res.send("Wrong Credentials");
+        }
+
+        //Token 
+        const token = jwt.sign(
+            {
+                userId: user.id,
+                name: user.name,
+                email: user.email,
+                roleId: user.role_id
+            },
+            'secreto'
+        );
+        return res.json(
+            {
+                success: true,
+                message: "Token created",
+                data: token
+            }
+        );
+    } catch (error) {
+
+        return res.status(500).json(
+            {
+                success: false,
+                message: "something went wrong",
                 error: error.message
             }
         );
