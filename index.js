18a7c3d04b445af72778947acb029e16842b0a43
jordijerico <jordijerico@gmail.com>
2023-05-07 18:31:05 +0200

    CORS removed to test deploy

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const db = require("./db");
 const app = express();
 const router = require('./router');
-const cors = require('cors');
+// const cors = require('cors');
 
-let corsOptions = {//CONFIGURO OPCIONES DE CORS
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  };
+// let corsOptions = {//CONFIGURO OPCIONES DE CORS
+//     origin: "*",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204
+//   };
   
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
@@ -20,7 +20,7 @@ app.get("/", (req,res) => {
 })
 
 
-const PORT = 5984;
+const PORT = 4000;
 
 db.then(() => {
     app.listen(PORT, () => console.log("Server on port " + PORT));
