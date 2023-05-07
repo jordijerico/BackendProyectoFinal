a3ebb5a37060bd21e6330d1fb426b3f7e321a992
jordijerico <jordijerico@gmail.com>
2023-05-07 18:46:22 +0200

    CORS added to test deploy

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const app = express();
 const router = require('./router');
 const cors = require('cors');
 
+<system.webServer>
+
+    <httpProtocol>
+        <customHeaders>
+            <add name="Access-Control-Allow-Origin" value="*" />
+            <add name="Access-Control-Allow-Headers" value="Content-Type" />
+            <add name="Access-Control-Allow-Methods" value="GET, POST, PUT, DELETE, OPTIONS" />
+        </customHeaders>
+    </httpProtocol>
+</system.webServer>
+
 let corsOptions = {//CONFIGURO OPCIONES DE CORS
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204
-  };
-  
+};
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     return res.send("Hola, página de prueba!")
 })
 
