a083cd8ba596516bf8e92eff4073b9d283e8b30b
jordijerico <jordijerico@gmail.com>
2023-05-07 18:32:26 +0200

    CORS added to test deploy

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const db = require("./db");
 const app = express();
 const router = require('./router');
-// const cors = require('cors');
+const cors = require('cors');
 
-// let corsOptions = {//CONFIGURO OPCIONES DE CORS
-//     origin: "*",
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204
-//   };
+let corsOptions = {//CONFIGURO OPCIONES DE CORS
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  };
   
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
