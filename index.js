f3c4146194ecbad29f4613f4614737e6e3bcb444
jordijerico <jordijerico@gmail.com>
2023-05-07 18:58:07 +0200

    changed index.js to fix CORS problem

--- index.js
+++ index.js
@@ -4,17 +4,7 @@ const app = express();
 const router = require('./router');
 const cors = require('cors');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
-let corsOptions = {//CONFIGURO OPCIONES DE CORS
-    origin: "*",
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
